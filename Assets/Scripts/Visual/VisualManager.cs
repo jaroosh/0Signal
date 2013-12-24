7e856423d4f204abb2bd9a7f0bdce2d3dbf5da3d
Jarek Wawro <jarek.wawro@gmail.com>
2013-12-24 13:32:02 +0100

    Simple structure

---
+++ Assets/Scripts/Visual/VisualManager.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+// Manager responsible for visual stuff, state transitions etc. 
+[RequireComponent (typeof (Animator))]
+public class VisualManager : MonoBehaviour {
+
+	public void SetState(string stateName) {
+	}
+}
